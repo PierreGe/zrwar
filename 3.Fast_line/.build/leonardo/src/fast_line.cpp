91fe635906f85c3ba2f9bb9d58d311726fc0c3cc
Mathieu Iko <mathieu.iko@gmail.com>
2014-05-03 04:22:03 +0200

    Fast-line: simpler turn when easy to catch

--- 3.Fast_line/.build/leonardo/src/fast_line.cpp
+++ 3.Fast_line/.build/leonardo/src/fast_line.cpp
@@ -22,13 +22,17 @@ void loop();
 //#include <Wire.h>
 
 #define DEVIATE_THRES 100
-#define SEARCH_SPEED 200
+#define SEARCH_SPEED 150
 #define MAX_SPEED 400
 
+#define DELAY_BEFORE_LOST 250
 #define DELAY_CONTINUE 150
 #define DELAY_BACK 160
 #define DELAY_SEARCH 0
 #define DELAY_AFTER_SPIN 200
+
+#define DELAY_BEFORE_TURN 50
+#define DELAY_TURN 75
 
 #define COMPASS_EPSILON 40
 
@@ -36,7 +40,9 @@ void loop();
 #define DEVIATE_EPSILON 10
 
 #define MAX_POSITION 5000
-#define POSITION_EPSILON 20
+#define POSITION_EPSILON 15
+#define MAX_SENSOR 1000
+#define SENSOR_EPSILON 10
 
 LSM303 compass;
 
@@ -52,6 +58,7 @@ unsigned int sensors[6];
 
 int position, deviate, side, search, seek_delay;
 int orientation, spinning;
+int turn_left, turn_right;
 
 
 void setup()
@@ -94,6 +101,8 @@ void setup()
 //  side = 1;
   search = 0;
   spinning = 0;
+  turn_left = 0;
+  turn_right = 0;
 
   // Wait for the user button to be pressed and released
 //  button.waitForButton();
@@ -109,50 +118,64 @@ void loop()
   // individual sensor readings
   position = reflectanceSensors.readLine(sensors);
 
-  if ((position < POSITION_EPSILON) || (position > MAX_POSITION - POSITION_EPSILON)) {
+  if (turn_left == 1) { // turn left!
+    turn_left = 0;
+    turn_right = 0;
+    leftspeed = -SEARCH_SPEED;
+    rightspeed = SEARCH_SPEED;
+    seek_delay = DELAY_TURN;
+  }
+  else if (turn_right == 1) { // turn right!
+    turn_left = 0;
+    turn_right = 0;
+    leftspeed = SEARCH_SPEED;
+    rightspeed = -SEARCH_SPEED;
+    seek_delay = DELAY_TURN;
+  }
+
+  else if (position > MAX_POSITION - POSITION_EPSILON) { // I'm lost! -> raw search
     switch (search) {
-      case 0: // First, search if this was just a line break
+      case 0: // first, see whether this was just a line break
         search = 1;
         leftspeed = rightspeed = SEARCH_SPEED;
         seek_delay = DELAY_CONTINUE;
         break;
-      case 1: // If not, go back
+      case 1: // if not, go back
         search = 2;
         leftspeed = rightspeed = -SEARCH_SPEED;
         seek_delay = DELAY_BACK;
         break;
-      case 2: // then choose a side
+      case 2: // then randomly choose a side
         search = 3;
         side = random(2) * 2 - 1;
         seek_delay = 0;
         break;
-      case 3: // and finally search
+      case 3: // and finally spin around to search
         search = 4;
         leftspeed = side * SEARCH_SPEED;
         rightspeed = (-side) * SEARCH_SPEED;
         seek_delay = 0;
         break;
       case 4: 
-/*        compass.read();*/
-/*        if (spinning && (abs(int(compass.heading()) - orientation) < COMPASS_EPSILON)) {*/
-/*          spinning = 0;*/
-/*          search = 5;*/
-/*          leftspeed = rightspeed = SEARCH_SPEED;*/
-/*          seek_delay = DELAY_AFTER_SPIN;*/
-/*        }*/
-/*        else if (!spinning && (abs(int(compass.heading()) - orientation) > COMPASS_EPSILON)) {*/
-/*          spinning = 1;*/
-/*        }*/
-/*        else {*/
-/*          spinning = 0;*/
-/*        }*/
         break;
           
       default:
         search = 0;
     }
   }
-  else {
+
+  else if (sensors[0] > MAX_SENSOR - SENSOR_EPSILON) { // turn left?
+    turn_left = 1;
+    turn_right = 0;
+    seek_delay = DELAY_BEFORE_TURN;
+  }
+  else if (sensors[5] > MAX_SENSOR - SENSOR_EPSILON) { // turn right?
+    turn_right = 1;
+    turn_left = 0;
+    seek_delay = DELAY_BEFORE_TURN;
+  }
+
+  else { // normal behavior, follow the line
     search = 0;
     deviate = 2500 - position;
 
@@ -166,20 +189,10 @@ void loop()
     }
     else {
       leftspeed = MAX_SPEED;
-      rightspeed = MAX_SPEED + deviate / DEVIATE_FACTOR;
+      rightspeed = MAX_SPEED;
     }
     seek_delay = 0;
   }
-
-/*  if (deviate > DEVIATE_THRES){*/
-/*    leftspeed = SPEED_LOW;*/
-/*    rightspeed = SPEED_HIGH;*/
-/*  } else if (deviate < -DEVIATE_THRES){*/
-/*    leftspeed = SPEED_HIGH;*/
-/*    rightspeed = SPEED_LOW;*/
-/*  } else {*/
-/*    leftspeed = rightspeed = SPEED_HIGH;*/
-/*  }*/
 
   motors.setSpeeds(leftspeed, rightspeed);
   delay(seek_delay);
